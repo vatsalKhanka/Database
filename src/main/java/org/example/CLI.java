22c4f9f7ecca60557a61ed124a09c2ac7f67a515
Vatsal <vatsalkhanka25@gmail.com>
2025-05-21 17:16:21 +0530

    we

--- src/main/java/org/example/CLI.java
+++ src/main/java/org/example/CLI.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CLI {
 
@@ -15,14 +17,15 @@ public class CLI {
     }
 
     public static void executeCommand(String command) {
+        command = command.replaceAll("\n", "");
         System.out.println(command);
-        String[] args = command.split(" ");
-        System.out.println(Arrays.toString(args));
-        if("create".equals(args[0])) {
-            String tableCommand = command.replace(args[0] + " ", "");
-            String name = tableCommand.substring(0, tableCommand.indexOf("("));
-            System.out.println(name);
-        }
+        Pattern createTable = Pattern.compile("(?i)create\\s+table\\s+(\\w+)");
+        Matcher matcher = createTable.matcher(command);
+        System.out.println(matcher.matches());
+        System.out.println(matcher.find());
+        System.out.println(matcher.groupCount());
+        matcher.matches();
+        System.out.println(matcher.group(1));
     }
 
 }
